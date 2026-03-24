7e442ebd0a09c9d644d52dc25ca68737a33abce4
yuchenyang83 <148430476+yuchenyang83@users.noreply.github.com>
2026-03-24 21:07:07 +0800

    Add files via upload

--- Code/Fig1.R
+++ Code/Fig1.R
@@ -11,9 +11,14 @@
 #   Output data files :  Data_and_Code/results/Figure1.eps
 #
 #   R Version   :    R-4.3.1
-#   Required R packages : ggplot2; ggtext; nlme; mvtnorm
+#   Required R packages : ggplot2; ggtext; nlme; mvtnorm; dplyr; scales; tidyr
 #
 ################################################################################
+library(grid)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(tidyr)
 actg398 <- read.table(paste0(PATH, "/Data/source/actg398.txt"), header = T)
 table(actg398$calwk)
 
@@ -83,15 +88,6 @@ for (i in 1:N)
 
 
 actg398.miss[which(actg398.miss$txday == -1 | actg398.miss$txday == 0), ]
-library(ggplot2)
-kk <- ggplot(data = actg398.miss, aes(x = txday, y = logrna, grop = as.factor(patid))) +
-  geom_line() +
-  xlab("Day") +
-  ylab("log(RNA)") +
-  ggtitle("actg398") +
-  theme(legend.position = "top") +
-  theme(text = element_text(size = 20))
-kk
 
 ## change to matrix
 n <- max(ni)
@@ -102,64 +98,181 @@ n <- length(Subject)
 nj <- numeric(n)
 for (i in 1:n) nj[i] <- length(actg398.miss$Dayt[actg398$Subject == Subject[i]])
 
+## observed data only
+obs <- actg398.miss %>%
+  filter(!is.na(logrna)) %>%
+  mutate(
+    calwk  = as.numeric(calwk),
+    trtarm = factor(trtarm, levels = c(0, 1), labels = c("Therapy", "Placebo"))
+  )
 
-ya1 <- ggplot(data = actg398.miss, aes(x = Time, y = logrna, group = as.factor(Subject))) +
-  geom_line(aes(group = as.factor(Subject))) +
-  geom_point() +
-  xlab("Time") +
-  ylab(NULL) +
-  ggtitle("actg398") +
-  theme(legend.position = "top") +
-  theme(text = element_text(size = 20))
-ya1
+day_levels <- ll
 
+obs <- obs %>%
+  mutate(
+    offset = ifelse(trtarm == "Therapy", -0.24, 0.24),
+    xpos   = calwk + offset
+  )
 
-library(ggplot2)
-ggplot() +
-  geom_boxplot(
-    data = actg398.miss[-which(is.na(actg398.miss$logrna)), ], aes(y = logrna, x = as.factor(calwk), group = as.factor(calwk)), alpha = 0,
-    outlier.colour = "red", outlier.shape = 2, outlier.size = 3, outlier.alpha = 1, linewidth = 0.9
+sum_arm <- obs %>%
+  group_by(trtarm, calwk) %>%
+  summarise(med = mean(logrna, na.rm = TRUE), .groups = "drop") %>%
+  mutate(offset = ifelse(trtarm == "Therapy", -0.4, 0.4), xpos   = calwk + offset)
+
+col_arm  <- c("Therapy" = "#9E1F1F", "Placebo" = "#1F4E79")
+fill_arm <- c("Therapy" = "#F3DEDE", "Placebo" = "#DCE6F1")
+
+theme_pub <- theme_classic(base_size = 16) +
+  theme(
+    axis.title = element_text(size = 18),
+    axis.text  = element_text(size = 14, color = "black"),
+    legend.position = "top",
+    legend.title = element_blank()
+  )
+
+p_top <- ggplot() +
+  geom_line(data = obs, aes(x = calwk, y = logrna, group = Subject),
+            color = "grey85",
+            linewidth = 0.35) +
+  geom_boxplot(data = obs, aes(x = xpos, y = logrna, group = interaction(calwk, trtarm), linetype = trtarm, fill = trtarm),
+               width = 1.5,
+               color = "grey30",
+               linewidth = 1.1,
+               show.legend = FALSE,
+               outlier.shape = 2,
+               outlier.fill = "red",
+               outlier.colour = "red3",
+               outlier.size = 2.8,
+               outlier.stroke = 0.9,
+               outlier.alpha = 1
   ) +
-  xlab("Month") +
-  ylab(expression(sqrt("CD4"))) +
-  theme(legend.position = "none") +
-  theme(text = element_text(size = 20))
+  geom_line(data = sum_arm, aes(x = xpos, y = med, group = trtarm, color = trtarm, linetype = trtarm), linewidth = 1.3) +
+  geom_point(data = sum_arm, aes(x = xpos, y = med, color = trtarm, shape = trtarm), size = 5.2, stroke = 1.2) +
+  scale_x_continuous(breaks = day_levels,labels = day_levels) +
+  scale_color_manual(values = col_arm) +
+  scale_shape_manual(values = c("Therapy" = 1, "Placebo" = 0)) +
+  scale_fill_manual(values = fill_arm) +
+  labs(x = "Week", y = expression(log[10](RNA))) +
+  theme(legend.key.width = unit(2.0, "cm")) +
+  guides(
+    color = guide_legend(keywidth = unit(2.0, "cm")),
+    shape = guide_legend(keywidth = unit(2.0, "cm")),
+    linetype = guide_legend(keywidth = unit(2.0, "cm"))) + 
+  theme_pub
+# p_top
+
+n_total_all <- actg398.miss %>%
+  distinct(Subject) %>%
+  nrow()
+
+bar_df <- actg398.miss %>%
+  filter(!is.na(logrna)) %>%
+  distinct(Subject, trtarm, Time) %>%
+  mutate(
+    trtarm = factor(trtarm, levels = c(0, 1), labels = c("Therapy", "Placebo")),
+    Time   = as.numeric(Time)
+  ) %>%
+  count(trtarm, Time, name = "n_obs") %>%
+  complete(
+    trtarm,
+    Time = day_levels,
+    fill = list(n_obs = 0)
+  ) %>%
+  mutate(
+    Time_f    = factor(Time, levels = day_levels),
+    prop_bar  = n_obs / n_total_all,
+    label_bar = sprintf("%.1f%%", 100 * prop_bar)
+  )
+
+line_df <- actg398.miss %>%
+  filter(!is.na(logrna)) %>%
+  distinct(Subject, Time) %>%
+  mutate(Time = as.numeric(Time)) %>%
+  count(Time, name = "n_obs_all") %>%
+  complete(Time = day_levels, fill = list(n_obs_all = 0)) %>%
+  mutate(prop_all   = n_obs_all / n_total_all, label_line = sprintf("%.1f%%", 100 * prop_all),
+         Time_f = factor(Time, levels = day_levels))
 
 
-library(ggplot2)
-kk <- ggplot() +
-  geom_line(data = actg398.miss[-which(is.na(actg398.miss$logrna)), ], aes(x = as.factor(calwk), y = logrna, group = as.factor(Subject))) +
-  geom_point(data = actg398.miss[-which(is.na(actg398.miss$logrna)), ], aes(x = as.factor(calwk), y = logrna, group = as.factor(Subject))) +
-  geom_boxplot(
-    data = actg398.miss[-which(is.na(actg398.miss$logrna)), ], aes(y = logrna, x = as.factor(calwk), group = as.factor(calwk)), alpha = 0,
-    outlier.colour = "red", colour = "red", lwd = 1, notchwidth = 0.5, outlier.shape = 2, outlier.size = 3, outlier.alpha = 1
+ymax_bottom <- max(c(bar_df$n_obs, line_df$n_obs_all)) * 1.08
+
+p_bottom <- ggplot(bar_df, aes(x = Time_f, y = n_obs, fill = trtarm, linetype = trtarm)) +
+  geom_col(
+    position = position_dodge(width = 0.72),
+    width = 0.62,
+    color = "grey35",
+    linewidth = 1.2
   ) +
-  xlab("Day") +
-  ylab(expression(log[10]("RNA"))) +
-  scale_x_discrete(breaks = ll) +
-  theme(legend.position = "none") +
-  theme(text = element_text(size = 20))
-kk
-
-
-k5 <- ggplot(data = actg398.miss[-which(is.na(actg398.miss$logrna)), ], aes(x = as.factor(Time))) +
-  geom_bar(position = position_dodge()) +
-  xlab("Time") +
-  ylab("Number of observed responses") +
   geom_text(
-    stat = "count", aes(label = paste0(round(after_stat(count) / n, 3) * 100, "%")), vjust = -0.1, color = "red", size = 7,
-    position = position_dodge(0.9)
+    aes(label = label_bar),
+    position = position_dodge(width = 0.72),
+    vjust = -0.35,
+    size = 2.5,
+    color = "red3"
   ) +
-  scale_y_continuous(limits = c(0, 500)) +
-  theme(legend.position = "top") +
-  scale_fill_discrete(name = NULL, labels = c("Placebo", "Therapy")) +
-  theme(text = element_text(size = 20))
-k5
+  geom_text(
+    aes(y = n_obs, label = n_obs),
+    position = position_dodge(width = 0.72),
+    vjust = 1.8,
+    size = 2.5,
+    color = "black"
+  ) +
+  geom_line(
+    data = line_df,
+    aes(x = Time_f, y = n_obs_all, group = 1),
+    inherit.aes = FALSE,
+    color = "black",
+    linewidth = 0.9
+  ) +
+  geom_point(
+    data = line_df,
+    aes(x = Time_f, y = n_obs_all),
+    inherit.aes = FALSE,
+    color = "black",
+    size = 2.6
+  ) +
+  geom_text(
+    data = line_df,
+    aes(x = Time_f, y = n_obs_all, label = label_line),
+    inherit.aes = FALSE,
+    vjust = -0.75,
+    size = 4.6,
+    color = "red3"
+  ) +
+  geom_text(
+    data = line_df,
+    aes(x = Time_f, y = n_obs_all, label = n_obs_all),
+    inherit.aes = FALSE,
+    vjust = 1.6,
+    size = 4.2,
+    color = "black"
+  ) +
+  scale_fill_manual(values = fill_arm) +
+  scale_y_continuous(
+    limits = c(0, ymax_bottom),
+    breaks = c(0, 144, 288, 432, 481),
+    name = "Number of observed responses",
+    sec.axis = sec_axis(
+      trans = ~ . / n_total_all,
+      breaks = c(0.30, 0.60, 0.90, 1.00),
+      labels = c("30%", "60%", "90%", "100%"),
+      name = "Observed proportion")) +
+  labs(x = "Week") +
+  theme_pub +
+  theme(
+    axis.title.y.left  = element_text(color = "black"),
+    axis.text.y.left   = element_text(color = "black"),
+    axis.ticks.y.left  = element_line(color = "black"),
+    axis.title.y.right = element_text(color = "red3"),
+    axis.text.y.right  = element_text(color = "red3"),
+    axis.ticks.y.right = element_line(color = "red3")
+  )
 
 source(paste0(PATH, "/function/multiplot.R"))
-layout <- matrix(c(1, 2), ncol = 1, byrow = TRUE)
-multiplot(plotlist = list(kk, k5), layout = layout)
+layout <- matrix(c(1, 2, 1, 2), ncol = 2, byrow = TRUE)
+multiplot(plotlist = list(p_top, p_bottom), layout = layout)
 
-postscript(paste0(PATH, "/Result/Figure1.eps"), width = 10, height = 8, paper = "special")
-multiplot(plotlist = list(kk, k5), layout = layout)
-dev.off()
+# postscript(paste0(PATH, "/Result/Figure1.eps"), width = 12, height = 8, paper = "special")
+# multiplot(plotlist = list(p_top, p_bottom), layout = layout)
+# dev.off()
+
--- Code/Fig2.R
+++ Code/Fig2.R
@@ -1,51 +1,84 @@
 ################################################################################
-#                                                                                     
-#   Filename    :    Fig2.R  												  
+#
+#   Filename    :    Fig2.R
 #   Project     :    BiomJ article "Robust HIV Viral Dynamics: A Nonlinear Mixed-Effects Framework for
 #                    Heavy-Tailed Data with Informative Dropout"
 #   Authors     :    Yu-Chen Yang and Tsung-I Lin and Luis M. Castro and Wan-Lun Wang
 #   Date        :    14.03.2026
 #   Purpose     :    produce Figure 2 for AIDS data
 #
-#   Input data files  :  Data_and_Code/Data/fixed_alpha.txt
-#   Output data files :  Data_and_Code/results/Figure2.eps
+#   Input data files  :  Data/fixed_alpha.txt
+#   Output data files :  Result/Figure2.eps
 #
-#   R Version   :    R-4.3.1                                                              
-#   Required R packages : ggplot2; ggtext; pROC; rlang; cowplot
+#   R Version   :    R-4.3.1
+#   Required R packages : ggplot2; dplyr; grid
 #
-################################################################################ 
-### ### ### ### ### ### ### ### ### 
-kk <- as.data.frame(read.table(paste0(PATH, "/Data/fixed_alpha.txt"), na.strings = "NA", sep = ""))
+################################################################################
+library(ggplot2)
+library(dplyr)
+library(grid)
 
-ya1 = ggplot(kk, aes(x=alpha, y=est.beta, color = "black")) + 
-  geom_point(size = 2) + geom_line(linewidth = 1) +
-  facet_wrap(.~beta, labeller = "label_parsed", scales = "free", ncol = 4) + 
-  xlab(expression(paste(alpha[2]))) + 
-  geom_line(data = kk, aes(x=alpha, y=est.upper, color = "red"), linetype = 2, linewidth = 1) +
-  geom_line(data = kk, aes(x=alpha, y=est.lower, color = "red"), linetype = 2, linewidth = 1) +
-  scale_x_continuous(breaks = c(-6,-4, -2, -1, 0, 1, 2, 4,6)) +
-  theme(legend.key.width = unit(2, "cm")) +
-  guides(shape = guide_legend(reverse=F,title=NULL),
-         linetype = guide_legend(reverse=F,title=NULL),
-         color = guide_legend(reverse=F,title=NULL),
-         fill = guide_legend(reverse=F,title=NULL)) +
-  theme(legend.position="top") +
-  scale_y_continuous(name = "Estimated fixed effects") +
-  scale_color_manual(values = c("black", "red"),labels = c("Estimated value", "95% Confidence bound")) + 
-  theme(text = element_text(size=25), 
-        strip.text.x = element_text(size = 25), 
-        axis.text.x = element_text(size = 20),
-        axis.title.x = element_text(size = 25)) + 
-  theme(axis.ticks.y.right = element_blank(),
-        axis.text.y.right = element_blank(), 
-        axis.title.y.right = element_text(color = "red", vjust = 2))
+kk <- read.table(paste0(PATH, "/Data/fixed_alpha.txt"), header = TRUE, na.strings = "NA")
+
+kk <- as.data.frame(kk)
+kk$beta <- gsub("^paste\\((beta\\[[0-9]+\\])\\)$", "\\1", as.character(kk$beta))
+
+kk$alpha <- as.numeric(kk$alpha)
+key_alpha <- c(-6, -4, -2, -1, -0.5, 0, 0.5, 1, 2, 4, 6)
+
+kk_key <- kk %>%
+  group_by(beta) %>%
+  group_modify(~{
+    do.call(
+      rbind,
+      lapply(key_alpha, function(a0) {
+        .x %>%
+          slice_min(order_by = abs(alpha - a0), n = 1, with_ties = FALSE)
+      })
+    )
+  }) %>%
+  ungroup() %>%
+  distinct(beta, alpha, .keep_all = TRUE)
+
+
+ya1 <- ggplot(kk, aes(x = alpha, y = est.beta)) +
+  geom_line(aes(color = "Estimated value"), linewidth = 0.8) +
+  geom_errorbar(data = kk_key, aes(ymin = est.lower, ymax = est.upper, color = "95% confidence interval"),
+                width = 0.9, linetype = 1, linewidth = 0.65) +
+  ## all points plotted on top
+  geom_point(aes(color = "Estimated value"), size = 3) +
+  facet_wrap(. ~ beta, labeller = label_parsed, scales = "free_y", ncol = 4) +
+  scale_x_continuous(breaks = c(-6, -4, -2, -1, 0, 1, 2, 4, 6)) +
+  scale_color_manual(
+    values = c("Estimated value" = "black", "95% confidence interval" = "#084594"),
+    breaks = c("Estimated value", "95% confidence interval"),
+    labels = c("Estimated value", "95% Confidence interval")
+  ) +
+  labs(x = expression(alpha[2]), y = "Estimated fixed effects", color = NULL) +
+  theme_bw(base_size = 24) +
+  theme(
+    legend.position = "top",
+    legend.key.width = unit(2.0, "cm"),
+    legend.text = element_text(size = 22),
+    strip.text.x = element_text(size = 24),
+    axis.text.x = element_text(size = 18),
+    axis.text.y = element_text(size = 18),
+    axis.title.x = element_text(size = 24),
+    axis.title.y = element_text(size = 24),
+    panel.grid.minor = element_blank()
+  ) +
+  guides(
+    color = guide_legend(
+      reverse = FALSE,
+      override.aes = list(
+        linewidth = c(0.9, 0.8),
+        shape = c(16, NA)
+      )
+    )
+  )
+
 print(ya1)
 
-source(paste(PATH, "/function/multiplot.R", sep = ""))
-layout <- matrix(c(1), nrow = 1, byrow = TRUE)
-multiplot(plotlist = list(ya1), layout = layout)
-
-postscript(paste0(PATH, "/Result/Figure2.eps"), width = 16, height = 10, paper = "special")
-multiplot(plotlist = list(ya1), layout = layout)
-dev.off()
-
+# postscript(paste0(PATH, "/Result/Figure2.eps"), width = 20, height = 10, paper = "special")
+# print(ya1)
+# dev.off()
